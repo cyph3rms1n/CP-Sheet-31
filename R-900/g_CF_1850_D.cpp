b5a7ac9db2afc022554ec4fa10403dfd831c4a20
Jyotish yadav <22bcs12392@cuchd.in>
2025-04-27 03:36:25 +0530

    Create g_CF_1850_D.cpp

---
+++ R-900/g_CF_1850_D.cpp
@@ -0,0 +1,109 @@
+// codeforces.com/problemset/problem/1850/D
+// D. Balanced Round
+
+/**
+ * Author: k7
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define int long long
+typedef long long ll;
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+// Utility Functions
+inline void fast_io() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+}
+
+//prime factorization 
+// void primeFactorization(int n){
+//     int count = 0
+//     while(n % 2 == 0){
+//         count ++;
+//         n /= 2;
+//     }
+//
+//     for(int i =3;i<sqrt(n);i+=2){
+//         while(n % i == 0){
+//             count ++;
+//             x /= i;
+//         }
+//     }
+//     return count;
+// }
+
+// Function to compute (base^exp) % mod using modular exponentiation
+// int mod_pow(int base, int exp, int mod) {
+//     int result = 1;
+//     base = base % mod;
+//     while (exp > 0) {
+//         if (exp % 2 == 1) {
+//             result = (result * base) % mod;
+//         }
+//         base = (base * base) % mod;
+//         exp /= 2;
+//     }
+//     return result;
+// }
+
+
+// bool sieve(int n) {
+//         bool primes[n + 1];
+//         fill(primes, primes + n + 1, true);
+
+//         primes[0] = primes[1] = false;
+
+//         for (int i = 2; i * i <= n; i++) {
+//                 if (primes[i]) {
+//                         for (int j = i * i; j <= n; j += i) {
+//                                 primes[j] = false;
+//                         }
+//                 }
+//         }
+//         if (primes[n]) return true;
+//         else return
+//         false;
+
+// }
+
+void solve() {
+        int n, k;
+        cin >> n >> k;
+        vector < int > arr(n);
+        for (int i = 0; i < n; i++) {
+                cin >> arr[i];
+        }
+
+        sort(arr.begin(), arr.end());
+        int maxi = 1;
+        int counter = 1;
+
+        for (int i = 1; i < n; i++) {
+                if (k >= abs(arr[i] - arr[i - 1])) {
+                        counter++;
+                } else {
+                        counter = 1;
+                }
+                maxi = max(maxi, counter);
+        }
+
+        cout << n - maxi << endl;
+
+}
+
+signed main() {
+        fast_io();
+        // Start Here
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+        return 0;
+}
