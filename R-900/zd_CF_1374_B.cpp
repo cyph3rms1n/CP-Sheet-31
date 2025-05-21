6d1fd3a792c739ac8d1ff67f7a0ac47bf505456e
Jyotish yadav <22bcs12392@cuchd.in>
2025-05-21 21:20:13 +0530

    Create zd_CF_1374_B.cpp

---
+++ R-900/zd_CF_1374_B.cpp
@@ -0,0 +1,110 @@
+// https://codeforces.com/problemset/problem/1374/B
+// B. Multiply by 2, divide by 6
+
+/**
+ * Author: k7
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define int long long
+typedef long long ll;
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+// Utility Functions
+inline void fast_io() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+}
+
+//prime factorization 
+// void primeFactorization(int n){
+//     int count = 0
+//     while(n % 2 == 0){
+//         count ++;
+//         n /= 2;
+//     }
+//
+//     for(int i =3;i<sqrt(n);i+=2){
+//         while(n % i == 0){
+//             count ++;
+//             x /= i;
+//         }
+//     }
+//     return count;
+// }
+
+// Function to compute (base^exp) % mod using modular exponentiation
+// int mod_pow(int base, int exp, int mod) {
+//     int result = 1;
+//     base = base % mod;
+//     while (exp > 0) {
+//         if (exp % 2 == 1) {
+//             result = (result * base) % mod;
+//         }
+//         base = (base * base) % mod;
+//         exp /= 2;
+//     }
+//     return result;
+// }
+
+
+// bool sieve(int n) {
+//         bool primes[n + 1];
+//         fill(primes, primes + n + 1, true);
+
+//         primes[0] = primes[1] = false;
+
+//         for (int i = 2; i * i <= n; i++) {
+//                 if (primes[i]) {
+//                         for (int j = i * i; j <= n; j += i) {
+//                                 primes[j] = false;
+//                         }
+//                 }
+//         }
+//         if (primes[n]) return true;
+//         else return
+//         false;
+
+// }
+
+void solve() {
+        int n;
+        cin >> n;
+
+        int three_count = 0;
+        int two_count = 0;
+
+        while (n > 0 && n % 3 == 0) {
+                three_count++;
+                n /= 3;
+        }
+
+        while (n > 0 && n % 2 == 0) {
+                two_count++;
+                n /= 2;
+        }
+
+        if (n > 1 || two_count > three_count) {
+                cout << -1 << endl;
+        }
+
+        else {
+                cout << three_count + (three_count - two_count) << endl;
+        }
+}
+
+signed main() {
+        fast_io();
+        // Start Here
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+        return 0;
+}
